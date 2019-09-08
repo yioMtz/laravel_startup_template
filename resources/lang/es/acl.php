5ac9b2fd4ba8bf22f004360d95d439c4b914106d
Yio M <mtz1701@gmail.com>
2019-09-07 22:03:08 -0700

    added spanish lang files

---
+++ resources/lang/es/acl.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Access control Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during access control messages and labels.
+    |
+    */
+
+    'acl_title' => 'Administrar permisos y roles de usuarios',
+    'title' => 'Roles y permisos de usuario',
+    'list_of_users' => 'Lista de usuarios',
+    'list_of_roles' => 'Lista de roles disponibles',
+    'list_of_permissions' => 'Lista de permisos disponibles',
+    'editRoles' => 'Roles y permisos',
+    'rolesBtn' => 'Administrar roles',
+    'permissionsBtn' => 'Administrar permisos',
+    'rolename' => 'Nombre de rol',
+    'permissions' => 'Permisos',
+    'manageAssignPermissions' => 'Administrar permisos de asignación',
+    'createRole' => 'Crear nueva función',
+    'newRole' => 'Crear nuevo rol',
+    'createPermission' => 'Crear nuevo permiso',
+    'newPermission' => 'Crear nuevo permiso',
+    'permissionName' => 'Nombre del permiso',
+    'asignar_usuario_roles_texto' => 'Verifique los roles para el usuario y cualquier permiso individual adicional no cubierto por los roles asignados',
+    'roles' => 'Roles',
+    'invalidRole' => 'El rol no está definido',
+    'permissions_updated' => 'Roles y permisos actualizados!',
+    'permissionsToRole' => 'Establecer permisos a rol',
+    'permissionByRole' => 'Permiso establecido por rol',
+    'execute_permissions_to_role_text' => 'Verifique los permisos que esta función proporcionará a los usuarios',
+
+
+
+];
