c87a075078ebff04840d7689ee0c39c2505719e0
Yio M <mtz1701@gmail.com>
2019-10-17 16:25:55 -0700

    enable pagination

--- resources/views/users/home.blade.php
+++ resources/views/users/home.blade.php
@@ -60,7 +60,7 @@
                         @endforeach
                     </tbody>
             </table>
-            {{-- {{ $users->links() }} --}}
+            {{ $users->links() }}
           </div>
     </div>
   </div>
