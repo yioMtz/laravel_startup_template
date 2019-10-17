7f56a989e26aa205317ac4e5f6f58562d3693da9
Yio M <mtz1701@gmail.com>
2019-10-17 15:53:27 -0700

    load dashboard by role

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,19 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $roles = Auth::user()->getRoleNames();
+        $role = $roles[0];
+       switch ($role) {
+           case 'admin':
+               return view('admin.adminHome');
+               break;
+           case 'recepcionista':
+               return view('home');
+               break;
+
+           default:
+               # code...
+               break;
+       }
     }
 }
